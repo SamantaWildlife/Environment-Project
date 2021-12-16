e3bd43702bfdf5fa337de1ef4b3912dbf285da7d
SamantaWildlife <sjnw@hotmail.co.uk>
2021-12-16 14:50:35 +0000

    home.html display on Flask http://127.0.0.1:5000/

--- climate/website/home.py
+++ climate/website/home.py
@@ -1,15 +1,39 @@
 #http://127.0.0.1:5000/ Home.html
-
-import flask
-from flask import Flask, render_template
+from flask import Flask,render_template,request,abort
+import json
+import urllib.request
 
 app = Flask(__name__)
-app.config["DEBUG"] = True
+def tocelcius(temp):
+    return str(round(float(temp) - 273.16,2))
 
-@app.route('/', methods=['GET'])
-def home():
-    return render_template('home.html')
+@app.route('/',methods=['POST','GET'])
+def weather():
+    api_key = '48a90ac42caa09f90dcaeee4096b9e53'
+    if request.method == 'POST':
+        city = request.form['city']
+    else:
+        city = 'london'
+
+    try:
+        source = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid='+api_key).read()
+    except:
+        return abort(404)
+    # converting json data to dictionary
+
+    list_of_data = json.loads(source)
+
+    # data for variable list_of_data
+    data = {
+        "country_code": str(list_of_data['sys']['country']),
+        "coordinate": str(list_of_data['coord']['lon']) + ' ' + str(list_of_data['coord']['lat']),
+        "temp": str(list_of_data['main']['temp']) + 'k',
+        "temp_cel": tocelcius(list_of_data['main']['temp']) + 'C',
+        "pressure": str(list_of_data['main']['pressure']),
+        "humidity": str(list_of_data['main']['humidity']),
+        "cityname":str(city),
+    }
+    return render_template('homeTest.html',data=data)
 
 if __name__ == '__main__':
     app.run(debug=True)
-
